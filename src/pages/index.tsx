edb3cda29c527d57c5fc513f195bec1d4c7b5135
€$¥ <dev@krk.finance>
2023-04-08 11:59:17 +0200

    cleaned unecessary code in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,15 @@
-// React
-import { useState, useEffect, useContext } from "react";
-// Solana
-import { useWallet } from "@solana/wallet-adapter-react";
-// Layout
+import { useContext } from "react";
 import Layout from "@/layout";
-// Views
 import HomeView from "@/views/home";
-// Contexts
 import { HolderContext } from "@/contexts/holder.context";
-// Types
-import type { NextPage } from "next";
 import { HolderEnum } from "@/constants";
+import type { NextPage } from "next";
 
 const Dashboard: NextPage = () => {
-  const wallet = useWallet();
   const { holder } = useContext(HolderContext);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    console.log(holder);
-    setLoading(false);
-  }, [wallet.publicKey, holder]);
-
   return (
     <Layout>
-      {!loading && holder === HolderEnum.Yay ? (
+      {holder === HolderEnum.Yay ? (
         <HomeView />
       ) : (
         <div className="flex h-full w-full items-center justify-center text-center">
