552154817a2fe20c2813ec05f54b328bf941dd71
€$¥ <dev@krk.finance>
2023-04-08 12:06:14 +0200

    gated /create

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,22 +2,13 @@ import { useContext } from "react";
 import Layout from "@/layout";
 import HomeView from "@/views/home";
 import { HolderContext } from "@/contexts/holder.context";
+import { WIP } from "@/components/wip";
 import { HolderEnum } from "@/constants";
 import type { NextPage } from "next";
 
 const Dashboard: NextPage = () => {
   const { holder } = useContext(HolderContext);
-  return (
-    <Layout>
-      {holder === HolderEnum.Yay ? (
-        <HomeView />
-      ) : (
-        <div className="flex h-full w-full items-center justify-center text-center">
-          Under Construction
-        </div>
-      )}
-    </Layout>
-  );
+  return <Layout>{holder === HolderEnum.Yay ? <HomeView /> : <WIP />}</Layout>;
 };
 
 export default Dashboard;
