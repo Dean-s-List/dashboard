615f8d495449f1dd65d3c7dfcf712feb78ab74b8
€$¥ <dev@krk.finance>
2023-04-23 18:22:02 +0200

    fixed loadSingleProject in @/pages/feedback/index.tsx

--- src/pages/feedback/index.tsx
+++ src/pages/feedback/index.tsx
@@ -2,22 +2,22 @@ import Layout from "@/layout";
 import { ReviewerFeedback } from "@/views/feedback/reviewer";
 import type { Projects } from "@/types";
 import type { NextPage, NextPageContext } from "next";
-import { getSingleProject } from "@/tools/supabase";
+import { loadSingleProject } from "@/tools/supabase";
 
 interface Props {
-  data: Projects | null;
+  data: Projects | null | undefined;
 }
 
 const FeedbackPage: NextPage<Props> = ({ data }) => (
   <Layout>
-    <ReviewerFeedback currentProject={data} />
+    <ReviewerFeedback currentProject={data!} />
   </Layout>
 );
 
 FeedbackPage.getInitialProps = async (ctx: NextPageContext): Promise<Props> => {
   const { id } = ctx.query;
   console.log("welcome on the page for feedback ", id);
-  const { data } = await getSingleProject(id as string);
+  const data = await loadSingleProject(id as string);
   console.log(data);
   return { data };
 };
